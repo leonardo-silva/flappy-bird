ad79373875ed5be3dba5d42cb5a5721274e02882
Leonardo Silva <leonardosilvaif@hotmail.com>
2021-10-11 16:03:13 -0300

    bird0 - Loading up the images

---
+++ bird0/main.lua
@@ -0,0 +1,41 @@
+push = require 'push'  -- Library to setup screen features
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- Loads the image files and stores them as objects
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
+
+function love.load()
+    -- This filter avoids blurring images
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Fifty Bird')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end  
+
+function love.resize(w, h)
+    push:resize(w, h)
+end    
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:start()
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT-16)
+    push:finish()
+end
